f7c8640b891edfa70d7f98ea7309eee4648eb88b
Nicholas Costa <nicholascostadev@gmail.com>
2023-06-27 23:19:17 -0300

    refactor: remove stories files + add rollup-dts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
 import scss from 'rollup-plugin-scss'
+import dts from 'rollup-plugin-dts'
 
 import packageJson from './package.json' assert { type: "json" };
 
@@ -22,5 +23,16 @@ export default [
       }),
       typescript({ tsconfig: "./tsconfig.json", clean: true }),
     ],
+  },
+  {
+    input: "./index.ts",
+    output: [{ file: "dist/types.d.ts", format: "es" }],
+    plugins: [
+      scss({
+        fileName: "bundle.css"
+      }),
+      typescript({ tsconfig: "./tsconfig.json", clean: true }),
+      dts()
+    ]
   }
 ];
