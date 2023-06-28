d018b9948a3b62b97bec5b8980ac33aec36d989d
Nicholas Costa <nicholascostadev@gmail.com>
2023-06-27 23:39:03 -0300

    refactor: add basic button component

--- stories/Button/Button.stories.ts
+++ stories/Button/Button.stories.ts
@@ -7,11 +7,10 @@ const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-  },
+  args: {
+    variant: "primary",
+    children: "Button"
+  }
 }
 
 export default meta
@@ -20,27 +19,12 @@ type Story = StoryObj<typeof Button>
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    variant: "primary"
   },
 }
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
-  },
-}
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-}
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    variant: "secondary"
   },
 }
--- stories/Button/Button.tsx
+++ stories/Button/Button.tsx
@@ -1,56 +1,27 @@
 import React from 'react'
-import './button.css'
+import { cva, type VariantProps } from 'class-variance-authority'
+import styles from './button.module.scss'
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large'
-  /**
-   * Button contents
-   */
-  label: string
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void
-}
+const buttonVariants = cva(styles.button, {
+  variants: {
+    variant: {
+      primary: styles.primary,
+      secondary: styles.secondary
+    }
+  },
+  defaultVariants: {
+    variant: "primary"
+  }
+})
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary'
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
+
+export const Button = ({variant, ...props}: ButtonProps) => {
   return (
     <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' '
-      )}
       {...props}
+      className={buttonVariants({ variant })}
     >
-      {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
     </button>
   )
 }
